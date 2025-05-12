05ded0c755203a4c2c07972c1450b55ea784ddc0
Matheus Bernardes <matheusbnas@gmail.com>
2025-05-12 19:35:57 -0300

    Fix process.stdout polyfill for googleapis

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+// Ensure process.stdout exists for googleapis
+window.process = window.process || {};
+window.process.stdout = window.process.stdout || {};
+window.process.stdout.isTTY = false;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
