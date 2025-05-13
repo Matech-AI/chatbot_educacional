7386c260a4a8cefe4a655fcef0c5961451298e4a
Matheus Bernardes <matheusbnas@gmail.com>
2025-05-13 00:50:13 -0300

    Fix process polyfill for googleapis

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,41 @@
 // Polyfill process and stdout for googleapis
-window.process = window.process || {};
-window.process.stdout = window.process.stdout || {};
-window.process.stderr = window.process.stderr || {};
-window.process.stdin = window.process.stdin || {};
-
-if (window.process) {
-  window.process.stdout.isTTY = false;
-  window.process.stderr.isTTY = false;
-  window.process.stdin.isTTY = false;
+if (!window.process) {
+  window.process = {
+    stdout: {
+      isTTY: false,
+      write: () => {},
+      end: () => {}
+    },
+    stderr: {
+      isTTY: false,
+      write: () => {},
+      end: () => {}
+    },
+    stdin: {
+      isTTY: false
+    },
+    env: {},
+    version: 'v16.0.0',
+    platform: 'browser'
+  };
+} else {
+  // Ensure all required properties exist even if process exists
+  window.process.stdout = window.process.stdout || {
+    isTTY: false,
+    write: () => {},
+    end: () => {}
+  };
+  window.process.stderr = window.process.stderr || {
+    isTTY: false,
+    write: () => {},
+    end: () => {}
+  };
+  window.process.stdin = window.process.stdin || {
+    isTTY: false
+  };
   window.process.env = window.process.env || {};
-  window.process.version = 'v16.0.0';
-  window.process.platform = 'browser';
+  window.process.version = window.process.version || 'v16.0.0';
+  window.process.platform = window.process.platform || 'browser';
 }
 
 import React from 'react';
